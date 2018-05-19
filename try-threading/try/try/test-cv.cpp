b32a416bb06c7fc43cb8b064b9f8f29313f90ea0
czhao <czhao@nanometrics.com>
2018-05-19 22:04:21 +0800

    Trace thread instance, join, cv

---
+++ try-threading/try/try/test-cv.cpp
@@ -0,0 +1,55 @@
+// try.cpp : Defines the entry point for the console application.
+//
+
+//class condition_variable;
+//
+//Condition variable
+//A condition variable is an object able to block the calling thread
+//until notified to resume.
+//
+//It uses a unique_lock(over a mutex) to lock the thread 
+//when one of its wait functions is called.The thread remains 
+//blocked until woken up by another thread that calls a notification
+//function on the same condition_variable object.
+//
+//Objects of type condition_variable always use unique_lock<mutex> to wait : 
+//for an alternative that works with any kind of lockable type, see condition_variable_any
+
+
+// condition_variable example
+#include <iostream>           // std::cout
+#include <thread>             // std::thread
+#include <mutex>              // std::mutex, std::unique_lock
+#include <condition_variable> // std::condition_variable
+
+std::mutex mtx;
+std::condition_variable cv;
+bool ready = false;
+
+void print_id(int id) {
+	std::unique_lock<std::mutex> lck(mtx);
+	while (!ready) cv.wait(lck);
+	// ...
+	std::cout << "thread " << id << '\n';
+}
+
+void go() {
+	std::unique_lock<std::mutex> lck(mtx);
+	ready = true;
+	cv.notify_all();
+}
+
+int main()
+{
+	std::thread threads[10];
+	// spawn 10 threads:
+	for (int i = 0; i<10; ++i)
+		threads[i] = std::thread(print_id, i);
+
+	std::cout << "10 threads ready to race...\n";
+	go();                       // go!
+
+	for (auto& th : threads) th.join();
+
+	return 0;
+}
